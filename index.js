b10a6cbd99b391283d32ad50c483f704e074a973
Zerahar <heinisaastamoinen@gmail.com>
2021-08-16 17:07:10 +0300

    f

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const app = express()
+
 app.use(express.json())
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
