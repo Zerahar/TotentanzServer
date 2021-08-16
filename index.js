4dd916dd8d5dab7462f73d51a3f8b55d3a15b16a
Zerahar <heinisaastamoinen@gmail.com>
2021-08-16 17:43:00 +0300

    scscs

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const app = express()
 
 app
-  // .use(express.json())
+  .use(express.json())
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 app.get('/', (req, res) => { res.send('Hello World') })
